d96ee5ee607208622837023273b4ee8c7b509216
家才 王 <jiacai_wang@qq.com>
2019-04-29 18:58:03 +0800

    plt.show阻塞问题

--- StereoVidComfort.py
+++ StereoVidComfort.py
@@ -10,19 +10,20 @@ cap = cv2.VideoCapture('./vid/zootopia.mkv')
 frameCount = cap.get(cv2.CAP_PROP_FRAME_COUNT)
 frameRate = cap.get(cv2.CAP_PROP_FPS)
 
-for frameID in range(int(frameRate), int(frameCount), int(frameRate/5)):
+for frameID in range(int(frameRate), int(frameCount), int(frameRate*100)):
     cap.set(cv2.CAP_PROP_POS_FRAMES, frameID)
     isSuccess, img = cap.read()
     if isSuccess:
+        cv2.namedWindow("img",cv2.WINDOW_NORMAL);
         cv2.imshow('img', img)
         imgL = np.split(img, 2, 1)[0]
         imgR = np.split(img, 2, 1)[1]
         cv2.waitKey(1)
         stereo = cv2.StereoSGBM_create(numDisparities=96, blockSize=7)
         disparity = stereo.compute(imgL, imgR)
-        plt.title("SGBM")
+        plt.title("DepthMap")
         plt.imshow(disparity)
-        plt.show()
+        plt.pause(0.1)
 
 
 
@@ -76,3 +77,14 @@ for frameID in range(int(frameRate), int(frameCount), int(frameRate/5)):
 
 #    plt.imshow(disparity)
 #    plt.show()
+# import numpy as np
+# import cv2
+# from matplotlib import pyplot as plt
+# 
+# imgL = cv2.imread('tsukuba_l.png',0)
+# imgR = cv2.imread('tsukuba_r.png',0)
+# 
+# stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
+# disparity = stereo.compute(imgL,imgR)
+# plt.imshow(disparity,'gray')
+# plt.show()
